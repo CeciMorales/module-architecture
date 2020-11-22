97ec387de9a61140ad755af0569f2c61d70cf7e8
ahiraaam <ahicruma23@gmail.com>
2020-11-22 15:19:20 -0600

    LoginFront

--- client/src/components/login/LoginDashboard.js
+++ client/src/components/login/LoginDashboard.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import LoginForm from './LoginForm';
-import learning from '../assets/learning2-itesm.png'
+import LoginForm from "./LoginForm";
+import learning from "../assets/learning2-itesm.png";
 
 const LoginDashboard = () => {
-    return (
-        <>
-            <div className="border border-light p-3 mt-4 mb-4 shadow-lg p-3 mb-5 bg-white rounded">
-                <div className="d-flex align-items-center justify-content-center wrapper">
-                    <div className="container">
-                            <div className="two-thirds">
-                                <h1>hola</h1>
-                            </div>
-                            <div className="one-hird">
-                                <h1>adios</h1>
-                            </div>
-                    </div>
-                </div>
-            </div>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <div className="login-container">
+        <div className="row">
+          <div className="col-8">
+            <img className="img-login" src={learning}></img>
+          </div>
+          <div className="col-4">
+            <LoginForm></LoginForm>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default LoginDashboard;
--- client/src/components/login/LoginForm.js
+++ client/src/components/login/LoginForm.js
@@ -1,11 +1,42 @@
-import React from 'react';
-
+import React from "react";
+import logoTec from "../assets/logoTec.png";
 const LoginForm = () => {
-    return (
-        <>
-            <h1>Login Form</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <img className="img-logoTec" src={logoTec}></img>
+
+      <form className="formLogin">
+        <p className="p-login">
+          Para iniciar sesión ingresa tu usuario y contraseña
+        </p>
+        <div className="form-group">
+          <input
+            class="form-control"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            class="form-control"
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <div className="div-names">
+        <p>Ahiram Cruz Martínez</p>
+        <p>Cecilia Morales Arriaga</p>
+        <p>Juan Carlos Pichardo Vicencio</p>
+      </div>
+    </>
+  );
+};
 
 export default LoginForm;
